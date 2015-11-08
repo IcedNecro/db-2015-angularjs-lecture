d353d8d61ffadf9e58e1eece787b35d6947570a1
Roman Statkevych <romanstatkevich@gmail.com>
2015-11-08 15:33:31 +0200

    stage 4

--- static/js/controller.js
+++ static/js/controller.js
@@ -11,16 +11,34 @@ application.controller('profile_controller', function($scope) {
 	$scope.skills = ['python', 'java','JS', 'angular.js']
 
 }).controller('search_controller', function($scope) {
-	$scope.studentsList = [
-		{
-			'total_name': 'Roman Statkevich',
-			'faculty': 'FICT',
-			'course': 3
-		},
-		{
-			'total_name': 'Vasiya Pupkin',
-			'faculty': 'PTI',
-			'course': 2
-		}
-	]
+	$scope.getByFilter = function(text_filter) {
+		var studentsList = [
+			{
+				'total_name': 'Roman Statkevich',
+				'faculty': 'FICT',
+				'course': 3
+			},
+			{
+				'total_name': 'Vasiya Pupkin',
+				'faculty': 'PTI',
+				'course': 2
+			}
+		];
+
+		var filter = text_filter;
+		var test = studentsList.map(function(currentValue, index, array) {
+			return currentValue.total_name.indexOf(filter) != -1 ?
+				currentValue : null;
+		})
+		test = test.reduce(function(previous, current, index) {
+			if(current!=null)
+				previous.push(current);
+			else 
+				previous.concat([]);
+			return previous;
+		}, [])
+		$scope.data = test;
+	}
+
+
 }) 
