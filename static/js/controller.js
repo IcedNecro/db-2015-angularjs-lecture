3635b50deb92a46dddc215661d698fa0dde59926
Roman Statkevych <romanstatkevich@gmail.com>
2015-11-08 11:23:36 +0200

    stage 2

--- static/js/controller.js
+++ static/js/controller.js
@@ -1,5 +1,12 @@
 var application = angular.module('app',[]);
 
 application.controller('profile_controller', function($scope) {
-	$scope.helloMessage = 'hello world!';
+	$scope.userData = {
+		username: 'IcedNecro',
+		firstName: 'Roman',
+		lastName: 'Statkevich',
+		birth_day: new Date(1996, 05, 08)
+	};
+
+	$scope.skills = ['python', 'java','JS', 'angular.js']
 })
