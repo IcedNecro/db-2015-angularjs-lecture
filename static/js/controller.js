4fe9f3ba38dfb80891e0e6762b8359c4bbf802db
Roman Statkevych <romanstatkevich@gmail.com>
2015-11-08 14:08:13 +0200

    stage_3

--- static/js/controller.js
+++ static/js/controller.js
@@ -9,4 +9,18 @@ application.controller('profile_controller', function($scope) {
 	};
 
 	$scope.skills = ['python', 'java','JS', 'angular.js']
-})
+
+}).controller('search_controller', function($scope) {
+	$scope.studentsList = [
+		{
+			'total_name': 'Roman Statkevich',
+			'faculty': 'FICT',
+			'course': 3
+		},
+		{
+			'total_name': 'Vasiya Pupkin',
+			'faculty': 'PTI',
+			'course': 2
+		}
+	]
+}) 
